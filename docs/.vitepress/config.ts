7cd64dcee37dbd6bb55e48aeb77e02a63b392435
Brett Dorrans <hello@lapidist.net>
2025-08-30 11:01:16 +0100

    docs: sync rules navigation (#264)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -33,8 +33,17 @@ export default {
             text: 'Design Token',
             items: [
               { text: 'colors', link: '/rules/design-token/colors' },
-              { text: 'typography', link: '/rules/design-token/typography' },
-              { text: 'spacing', link: '/rules/design-token/spacing' }
+              { text: 'line-height', link: '/rules/design-token/line-height' },
+              { text: 'font-weight', link: '/rules/design-token/font-weight' },
+              { text: 'letter-spacing', link: '/rules/design-token/letter-spacing' },
+              { text: 'border-radius', link: '/rules/design-token/border-radius' },
+              { text: 'border-width', link: '/rules/design-token/border-width' },
+              { text: 'box-shadow', link: '/rules/design-token/box-shadow' },
+              { text: 'duration', link: '/rules/design-token/duration' },
+              { text: 'spacing', link: '/rules/design-token/spacing' },
+              { text: 'font-size', link: '/rules/design-token/font-size' },
+              { text: 'font-family', link: '/rules/design-token/font-family' },
+              { text: 'z-index', link: '/rules/design-token/z-index' }
             ]
           }
         ]
