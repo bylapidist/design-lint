17c4fe03983efa771fcd6e461eca729381ecceaa
Brett Dorrans <hello@lapidist.net>
2025-08-30 12:26:16 +0100

    ci: publish docs to github pages (#2)
    * ci: publish docs
    
    * chore: add changeset

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,6 @@
-export default {
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
   title: 'design-lint',
   description: 'Design system linter',
   themeConfig: {
@@ -7,7 +9,7 @@ export default {
       { text: 'Configuration', link: '/configuration' },
       { text: 'API', link: '/api' },
       { text: 'Plugins', link: '/plugins' },
-      { text: 'Rules', link: '/rules/' }
+      { text: 'Rules', link: '/rules/' },
     ],
     sidebar: [
       {
@@ -16,8 +18,8 @@ export default {
           { text: 'Usage', link: '/usage' },
           { text: 'Configuration', link: '/configuration' },
           { text: 'API', link: '/api' },
-          { text: 'Plugins', link: '/plugins' }
-        ]
+          { text: 'Plugins', link: '/plugins' },
+        ],
       },
       {
         text: 'Rules',
@@ -25,9 +27,12 @@ export default {
           {
             text: 'Design System',
             items: [
-              { text: 'component-usage', link: '/rules/design-system/component-usage' },
-              { text: 'deprecation', link: '/rules/design-system/deprecation' }
-            ]
+              {
+                text: 'component-usage',
+                link: '/rules/design-system/component-usage',
+              },
+              { text: 'deprecation', link: '/rules/design-system/deprecation' },
+            ],
           },
           {
             text: 'Design Token',
@@ -35,19 +40,28 @@ export default {
               { text: 'colors', link: '/rules/design-token/colors' },
               { text: 'line-height', link: '/rules/design-token/line-height' },
               { text: 'font-weight', link: '/rules/design-token/font-weight' },
-              { text: 'letter-spacing', link: '/rules/design-token/letter-spacing' },
-              { text: 'border-radius', link: '/rules/design-token/border-radius' },
-              { text: 'border-width', link: '/rules/design-token/border-width' },
+              {
+                text: 'letter-spacing',
+                link: '/rules/design-token/letter-spacing',
+              },
+              {
+                text: 'border-radius',
+                link: '/rules/design-token/border-radius',
+              },
+              {
+                text: 'border-width',
+                link: '/rules/design-token/border-width',
+              },
               { text: 'box-shadow', link: '/rules/design-token/box-shadow' },
               { text: 'duration', link: '/rules/design-token/duration' },
               { text: 'spacing', link: '/rules/design-token/spacing' },
               { text: 'font-size', link: '/rules/design-token/font-size' },
               { text: 'font-family', link: '/rules/design-token/font-family' },
-              { text: 'z-index', link: '/rules/design-token/z-index' }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-};
+              { text: 'z-index', link: '/rules/design-token/z-index' },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
