58751ba2f90ea06348311c677678a4fe5c0c31b7
Brett Dorrans <hello@lapidist.net>
2025-09-07 00:35:50 +0100

    chore: add changeset for tempy tmp util (#103)

--- src/utils/tmp.ts
+++ src/utils/tmp.ts
@@ -1,18 +1,12 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import os from 'node:os';
+import { temporaryDirectory } from 'tempy';
 
 /**
- * Create a temporary directory and return its resolved path.
+ * Create a temporary directory using {@link https://github.com/sindresorhus/tempy | tempy}.
+ *
  * @param prefix Directory name prefix.
  * @returns Absolute path to the created directory.
  */
 export function makeTmpDir(prefix = 'designlint-') {
-  const base = os.tmpdir();
-  const p = fs.mkdtempSync(path.join(base, prefix));
-  if (fs.realpathSync.native) {
-    return fs.realpathSync.native(p);
-  }
-  /* c8 ignore next */
-  return fs.realpathSync(p);
+  const sanitizedPrefix = prefix.replace(/[-_]+$/, '');
+  return temporaryDirectory({ prefix: sanitizedPrefix });
 }
