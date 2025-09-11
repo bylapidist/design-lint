0f89b20f760fbc5bfc5dca66f4958149a729dd5d
Brett Dorrans <hello@lapidist.net>
2025-09-11 18:39:03 +0100

    chore(utils): add collections index and changeset (#283)

---
+++ src/utils/collections/index.ts
@@ -0,0 +1,2 @@
+export * from './is-array.js';
+export * from './to-array.js';
--- tests/utils/is-array.test.ts
+++ tests/utils/is-array.test.ts
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { isArray } from '../../src/utils/is-array.js';
+import { isArray } from '../../src/utils/collections/index.js';
 
 void test('isArray detects arrays', () => {
   assert.equal(isArray([]), true);
